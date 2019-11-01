f21512e62f36cd1ef9147c07a4cc6126ba1822fa
Victor Toon <araujo.victor200@gmail.com>
2019-11-01 18:20:28 -0300

    read_ultrasonic_sensor feito

--- soul.s
+++ soul.s
@@ -21,7 +21,7 @@ _start:
 #Configurar o torque dos dois motores para zero;
     la t0, 0xFFFF0018
     sh zero, 0(t0) #coloco o motor 2 pra zero
-    
+
     la t0, 0xFFFF001A
     sh zero, 0(t0) #coloco o motor 1 pra zero
 #Configurar a cabeca do uóli
@@ -42,18 +42,69 @@ _start:
     sw t1, 0(t0) #salvo no endereço de memoria
 
 #------------------------------------------------------------------------------------------------------------------------#
+#Habilitar interrupções;
+#Configurar a pilha do usuário e do sistema;
+#Mudar para o modo usuário;
+#Desviar o fluxo para a função main do programa do usuário;
+aplicacao_de_controle:
+#Habilitando interrupções globais
+    csrr t0, mstatus #Bit 7
+    ori t0, t0, 0x80
+    csrw mstatus, t0
+
+#Habilitando interrupções externas
+    csrr t0, mie #Bit 11
+    ori t0, t0, 0x800
+    csrw mie, t0
+#Ajustando o mscratch
+    la t0, reg_buffer
+    csrw mscratch, t0 
+#Mudar pro modo usuário
+    csrr t0, mstatus #seta os bits 11 e 12
+    li t1, ~0x1800
+    and t0, t0, t1 #isso aqui vai ter o mstatus antigo com o 11 e o 12 sendo 0
+    csrw mstatus, t0 #guardo de volta
+
+    la t0, main
+    csrw mepc, t0
+
+    mret #me retorna pra func do usuario, que seria a main
+
+#------------------------------------------------------------------------------------------------------------------------#
 #Parametros: nenhum
 #Retorno: a0: Valor obtido na leitura do sensor; -1 caso nenhum objeto tenha sido detectado a menos de 600 centímetros.
 read_ultrasonic_sensor:
 la t0, 0xFFFF0020
 sw zero, 0(t0) # 
-#TODO: interromper até FFFF0020 ter o valor 1 e pegar os objetos do sensor
+RUS_While:
+    #empilha
+    addi sp, sp, -4
+    sw ra, 0(sp)
+
+    jal delay
+
+    #desempilha
+    lw ra, 0(sp)
+    addi sp, sp, 4
+
+    la t0, 0xFFFF0020
+    li t1, 1
+
+    bne t0, t1, RUS_While # if t0 != t1 then RUS_While
+
+la t0, 0xFFFF0024
+lw t0, 0(t0)
+li t1, 601
+
+blt t0, t1, RUS_600menor # if t0 < t1 then RUS_600menor
+li a0, -1
+ret
+
+RUS_600menor:
+mv  a0, t0 # a0 = t0
+ret
 
 #------------------------------------------------------------------------------------------------------------------------#
-
-
-
-
 
 
 
@@ -65,32 +116,40 @@ tratador_interrupcoes:
 
 delay:
     #empilha
-    addi sp, sp, -4
+    addi sp, sp, -12
     sw ra, 0(sp)
+    sw a0, 4(sp)
+    sw a1, 8(sp)
 
     jal time_now
 
     #desempilha
     lw ra, 0(sp)
-    addi sp, sp, 4
+    lw a0, 4(sp)
+    lw a1, 8(sp)
+    addi sp, sp, 12
 
-    addi s1, a0, 0; # s1 = a0 + 0 #tempo de referencia
-    li s0, 0 #tempo_atual=0
-    li s9, 1
+    addi a2, a0, 0; # a2 = a0 + 0 #tempo de referencia
+    li a1, 0 #tempo_atual=0
+    li a3, 1
     while:
         #empilha
-        addi sp, sp, -4
+        addi sp, sp, -12
         sw ra, 0(sp)
+        sw a0, 4(sp)
+        sw a1, 8(sp)
 
         jal time_now
 
         #desempilha
         lw ra, 0(sp)
-        addi sp, sp, 4
+        lw a0, 4(sp)
+        lw a1, 8(sp)
+        addi sp, sp, 12
 
-        addi s0, a0, 0; # s0 = a0 + 0
-        sub t0, s0, s1 # t0 = s0 - s1
-        blt t0, s9, while # if t0 < 1 then target
+        addi a1, a0, 0; # a1 = a0 + 0
+        sub t0, a1, a2 # t0 = a1 - a2
+        blt t0, a3, while # if t0 < 1 then target
 
     ret
 
